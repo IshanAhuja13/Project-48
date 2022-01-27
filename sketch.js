cf3ca835e75dbe883dcc600e2fe3806f86314298
IshanAhuja13 <76620278+IshanAhuja13@users.noreply.github.com>
2022-01-27 18:56:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var blastImage;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles;
+var cars = [];
+var tollCenter, tollCenterImage;
+var goldCoins = 0;
+
+function preload() {
+  backgroundImage = loadImage("./assets/background 3.jpg");
+  car1_img = loadImage("../assets/White Boat.png");
+  car2_img = loadImage("../assets/Red Boat.png");
+  track = loadImage("../assets/background 4.jpg");
+  fuelImage = loadImage("./assets/fuel.png");
+  powerCoinImage = loadImage("./assets/goldCoin.png");
+  obstacle1Image = loadImage("./assets/obstacle1.png");
+  obstacle2Image = loadImage("./assets/obstacle2.png");
+  lifeImage = loadImage("./assets/life.png");
+  blastImage = loadImage("./assets/blast.png");
+  tollCenterImage = loadImage("./assets/Toll Center.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+
+  if (gameState === 2) {
+    game.showLeaderboard();
+    game.end();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
